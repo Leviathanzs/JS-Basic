53c0afa9a794bf47f2660c25c3b546e2492a31bb
Leviathanzs <okywidhiansyah@gmail.com>
2022-07-11 13:02:52 +0700

    Try to fix it but still failed

--- Exercises/game.js
+++ Exercises/game.js
@@ -3,13 +3,12 @@ while(repeat) {
     //computer
     //var p = prompt('Tebak angka 1-10');
     var com = Math.floor((Math.random() * 10) + 1);
-    var p =  prompt(parseInt('Tebak angka 1-10'))
-    var x = 3;
+    var p =  parseInt(prompt('Tebak angka 1-10'));
     
     //rules
     var hasil = '';
     if(com == p) {
-        hasil = 'Benar!'
+        hasil = 'Benar!';
     } else if(p > com) {
         hasil = 'Terlalu besar';
     } else if(p < com) {
--- Exercises/script.js
+++ Exercises/script.js
@@ -1,20 +1,14 @@
 var s = '';
-var b = 3;
+var x = 5;
 
-for(var h = 1; h <= b; h++){
- for(var i = 1; i <= b - h; i++){
+for(var h = 0; h < x ; h++){
+ for(var i = 0; i < h; i++){
   s += ' ';
  }
- for(var j = 1; j < 2 * h; j++){
-  s += '*';
+ for(var j = x; j > h; j--){
+  s += ' *';
  }
  s += '\n';   
 }
 
 console.log(s);
-
-// Alur Kerja for : cek kondisi pertama jika true masuk ke for bawahnya,
-// for-2 dijalankan hingga false, kemudian lanjut for-3
-// for-3 dikerjakan hingga false, kemudian kerjakan kondisi for-1, 
-//jika for-1 true dan telah dijalankan \n, kemudian jalankan fungsin sebelum \n dimana for-1 kemudian for-2,
-// for-2 dan 3 dijalankan lagi dengan increasmentnya di reset ke default
