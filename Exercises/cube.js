7c0f43867b990dc6eb481ad082cbd4741140c776
Leviathanzs <okywidhiansyah@gmail.com>
2022-07-15 10:11:42 +0700

    Fixed Cube Volume Total

--- Exercises/cube.js
+++ Exercises/cube.js
@@ -1,9 +1,10 @@
-function cube(sisi1, sisi2) {
-    sisi1^3;
-    sisi2^3;
+function cubeVolumeTotal(sisi1, sisi2) {
+    
+    volume1 = sisi1 * sisi1 * sisi1;
+    volume2 = sisi2 * sisi2 * sisi2;
 
-    total = sisi1 + sisi2
+    total = volume1 + volume2
     return total;
 }
 
-console.log(cube(8,3));
+console.log(cubeVolumeTotal(4,3));
